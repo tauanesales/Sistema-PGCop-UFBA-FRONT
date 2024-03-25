e9c65624bfa6d440215896d1e25d9df4a5ea5be8
Tauane Sales <taysales6@gmail.com>
2024-03-25 03:20:05 -0300

    [UPT] Update importing pattern

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 
-import { router } from "./routes";
+import { router } from "@/app/routes";
 
 export default function App() {
   return <RouterProvider router={router} />;
