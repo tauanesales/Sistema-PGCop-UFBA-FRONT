8c4791087eb290e6b978033fe436437f4ad31780
Pablo Henrique <pablohenrique.r.c@gmail.com>
2024-04-04 11:13:33 -0300

    Adiciona token nas requisições com interceptor

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -4,4 +4,19 @@ import { useTokensStore } from "../../store/tokens";
 
 const api = axios.create({ baseURL: process.env.API_URL });
 
+api.interceptors.request.use(
+  (config) => {
+    const accessToken = useTokensStore.getState().accessToken;
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
 export default api;
