c6b33a0e021c117c1b15d2494374b374e527247d
Pablo Henrique <pablohenrique.r.c@gmail.com>
2024-04-04 10:49:54 -0300

    Adiciona regras de ordenação de imports no ESLInt

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,12 +13,17 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "simple-import-sort", "import"],
   rules: {
     "react/jsx-no-target-blank": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
   },
 };
