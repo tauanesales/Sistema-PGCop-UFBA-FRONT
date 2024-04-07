5421fb57b8b2f84f6f298ea4ea1c8155a5fd8314
Pablo Henrique <pablohenrique.r.c@gmail.com>
2024-04-07 20:14:49 -0300

    Ajusta uso de variáveis de ambiente no Vite

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 import { useTokensStore } from "../../store/tokens";
 
-const api = axios.create({ baseURL: process.env.API_URL });
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
 api.interceptors.request.use(
   (config) => {
