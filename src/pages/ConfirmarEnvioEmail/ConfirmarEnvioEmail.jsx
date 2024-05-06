e11659f0a15df6f0abce75db5a94e660d1c87ae9
Letícia Costa <leticiamc@ufba.br>
2024-05-06 14:24:47 -0300

    Tela Confirmar Cadastro  atualizada com responsividade

--- src/pages/ConfirmarEnvioEmail/ConfirmarEnvioEmail.jsx
+++ src/pages/ConfirmarEnvioEmail/ConfirmarEnvioEmail.jsx
@@ -5,14 +5,13 @@ const ConfirmarEnvioEmail = () => {
   const logoPgcop = "assets/logoPgcop.png";
 
   return (
-    <div className='containerConfirmarEnvioEmail'>
+    <div className='container'>
       <div className='containerCard gap'>
         <img src={logoPgcop} width={130} />
         <h1>Redefinir senha</h1>
         <div>
           <p>Enviamos um link para redefinição de senha para o e-mail informado.</p>
           <p>Por favor, verifique sua caixa de entrada.</p>
-
         </div>
         
         <ButtonSecondary link={'/'} label={"Voltar"} width={"12em"} />
--- src/pages/EsqueciSenha/EsqueciSenha.jsx
+++ src/pages/EsqueciSenha/EsqueciSenha.jsx
@@ -6,12 +6,12 @@ const EsqueciSenha = () => {
   const logoPgcop = "assets/logoPgcop.png";
 
   return (
-    <div className='containerEsqueciSenha'>
+    <div className='container'>
       <div className='containerCard'>
         <img src={logoPgcop} width={130} />
         <h1>Redefinir senha</h1>
         <p>Informe o e-mail cadastrado para redefinir sua senha de acesso.</p>
-        
+
         <div className='containInput'>
           <Input label={"E-mail:"} type={"email"} margin />
           <ButtonSecondary link={'/confirmar-envio-email'} label={"Entrar"} width={"12em"} />
