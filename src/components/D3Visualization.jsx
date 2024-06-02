c9ed76fbf5408e6880168d9099966dbb054e2d1e
phdiamantino <phdiamantinoo@gmail.com>
2024-06-02 19:14:09 -0300

    [ADD] Add Vis no projeto visual atualizado e add aviso 'ainda não há tarefas realizadas'

---
+++ src/components/D3Visualization.jsx
@@ -0,0 +1,188 @@
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+
+const D3Visualization = ({ dataDeInicio, dataFinal, dataAtual, tarefas }) => {
+  const svgRef = useRef();
+
+  useEffect(() => {
+    // Dimensões container
+    const margin = { top: 0, right: 40, bottom: 60, left: 40 };
+    const width = 1200 - margin.left - margin.right;
+    const height = 150 - margin.top - margin.bottom;
+
+    // Construção da escala horizontal e preparação para zoom
+    let currentScale = "mes";
+    let timeInterval = d3.timeMonth.every(1);
+    let timeFormat = "%b %Y";
+
+    const timeScale = d3.scaleTime()
+      .domain([dataDeInicio, dataFinal])
+      .range([0, width]);
+
+    function updateTimeScale(scale) {
+      switch (scale) {
+        case "ano":
+          timeInterval = d3.timeYear.every(1);
+          timeFormat = "%Y";
+          break;
+        case "mes":
+          timeInterval = d3.timeMonth.every(1);
+          timeFormat = "%b %Y";
+          break;
+        case "semana":
+          timeInterval = d3.timeWeek.every(1);
+          timeFormat = "%b %d, %Y";
+          break;
+        default:
+          break;
+      }
+      const ticks = timeScale.ticks(timeInterval);
+
+      svg.selectAll(".date-mark").remove();
+      svg.selectAll(".date-text").remove();
+
+      // Ticks e legenda escala horizontal
+      svg.selectAll(".date-mark")
+        .data(ticks)
+        .enter().append("line")
+        .attr("class", "date-mark")
+        .attr("x1", d => timeScale(d))
+        .attr("y1", height)
+        .attr("x2", d => timeScale(d))
+        .attr("y2", height + 7)
+        .style("stroke", "grey")
+        .style("stroke-width", 1);
+
+      svg.selectAll(".date-text")
+        .data(ticks)
+        .enter().append("text")
+        .attr("class", "date-text")
+        .attr("x", d => timeScale(d))
+        .attr("y", height + 20)
+        .attr("transform", d => `rotate(-30, ${timeScale(d)}, ${height + 20})`)
+        .text(d3.timeFormat(timeFormat))
+        .style("fill", "grey")
+        .style("font-size", "10px")
+        .style("text-anchor", "middle");
+    }
+
+    // Zoom e pan escala horizontal
+    const zoom = d3.zoom()
+      .scaleExtent([1, 100])
+      .translateExtent([[0, 0], [width, height]])
+      .extent([[0, 0], [width, height]])
+      .on("zoom", zoomed);
+
+    function zoomed(event) {
+      const transform = event.transform;
+      const newXScale = transform.rescaleX(timeScale);
+
+      svg.selectAll(".date-mark")
+        .attr("x1", d => newXScale(d))
+        .attr("x2", d => newXScale(d));
+      svg.selectAll(".date-text")
+        .attr("x", d => newXScale(d))
+        .attr("transform", d => `rotate(-30, ${newXScale(d)}, ${height + 20})`);
+      svg.selectAll(".tarefa-a-fazer")
+        .attr("x", (tarefa, index) => {
+          const prazo = new Date(dataDeInicio.getFullYear(), dataDeInicio.getMonth() + tarefa.prazoMeses, dataDeInicio.getDate());
+          return newXScale(prazo) - 10 + (index * 5);
+        });
+
+      svg.selectAll(".barra-progresso")
+        .attr("width", width * transform.k)
+        .attr("transform", `translate(${transform.x},0)`);
+
+      svg.selectAll(".progresso")
+        .attr("width", progressoWidth * transform.k)
+        .attr("transform", `translate(${transform.x},0)`);
+    }
+
+    const svg = d3.select(svgRef.current)
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .attr("cursor", "ew-resize")
+      .call(zoom)
+      .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+    // Linha escala horizontal
+    svg.append("line")
+      .attr("x1", 0)
+      .attr("y1", height)
+      .attr("x2", width)
+      .attr("y2", height)
+      .style("stroke", "grey")
+      .style("stroke-width", 1);
+
+    updateTimeScale(currentScale);
+
+    // Adiciona ícones de tarefa conforme a escala
+    svg.selectAll(".tarefa-a-fazer")
+      .data(tarefas)
+      .enter().append("image")
+      .attr("class", "tarefa-a-fazer")
+      .attr("cursor", "pointer")
+      .attr("x", (tarefa, index) => {
+        const prazo = new Date(dataDeInicio.getFullYear(), dataDeInicio.getMonth() + tarefa.prazoMeses, dataDeInicio.getDate());
+        return timeScale(prazo) - 10 + (index * 5);
+      })
+      .attr("y", height - 60)
+      .attr("width", 30)
+      .attr("height", 30)
+      .attr("xlink:href", (tarefa) => {
+        const prazo = new Date(dataDeInicio.getFullYear(), dataDeInicio.getMonth() + tarefa.prazoMeses, dataDeInicio.getDate());
+        return tarefa.feita ? "/assets/flagGreen.png" : (prazo < dataAtual ? "/assets/flagRed.png" : "/assets/flagBlack.png");
+      })
+      .on("mouseover", function (event, d) {
+        const prazo = new Date(dataDeInicio.getFullYear(), dataDeInicio.getMonth() + d.prazoMeses, dataDeInicio.getDate());
+        const tooltip = d3.select("#tooltip");
+        tooltip
+          .style("display", "block")
+          .html(`<strong>${d.nome}</strong><br>Data Limite: ${prazo.toLocaleDateString()}`)
+          .style("left", (event.pageX + 10) + "px")
+          .style("top", (event.pageY - 60) + "px")
+          .style("position", "absolute");
+      })
+      .on("mouseout", function () {
+        d3.select("#tooltip").style("display", "none");
+      });
+
+    // Barra temporal curso
+    svg.append("rect")
+      .attr("class", "barra-progresso")
+      .attr("x", 0)
+      .attr("y", height - 25)
+      .attr("rx", 5)
+      .attr("ry", 5)
+      .attr("width", width)
+      .attr("height", 12)
+      .style("fill", "none")
+      .style("stroke", "black")
+      .style("stroke-width", 0.3);
+
+    // Barra de progresso
+    const progresso = svg.append("rect")
+      .attr("class", "progresso")
+      .attr("x", 0)
+      .attr("y", height - 25)
+      .attr("rx", 5)
+      .attr("ry", 5)
+      .attr("width", 0)
+      .attr("height", 12)
+      .style("fill", "#84bf68");
+
+    const tempoDecorrido = dataAtual - dataDeInicio;
+    const progressoPorcentagem = (tempoDecorrido / (dataFinal - dataDeInicio));
+    const progressoWidth = progressoPorcentagem * width;
+    progresso.attr("width", progressoWidth);
+
+    return () => {
+      d3.select(svgRef.current).selectAll("*").remove();
+    };
+  }, [dataAtual, tarefas]);
+
+  return <svg ref={svgRef}></svg>;
+};
+
+export default D3Visualization;
--- src/pages/PerfilAluno/PerfilAluno.jsx
+++ src/pages/PerfilAluno/PerfilAluno.jsx
@@ -1,17 +1,20 @@
-import { useState, useEffect } from "react"; 
+import { useState, useEffect, useRef } from "react";
 import "./styles.css";
-
 import { MdEditNote, MdLogout } from 'react-icons/md';
 import { AiOutlineEdit , AiOutlineFileExcel } from 'react-icons/ai'; 
+import * as d3 from "d3"; // Importando D3.js
+import D3Visualization from "../../components/D3Visualization"; // Import Vis
+
+
 
 function PerfilAluno() {
   
   const logoPgcop = "/assets/logoPgcop.png";
+  const flagBlack = "/assets/flagBlack.png";
+  const flagGreen = "/assets/flagGreen.png";
+  const flagRed = "/assets/flagRed.png";
 
-  const dataDeInicio = new Date("2023-03-01"); // Data de Início do aluno
-
-  const [dataAtual, setDataAtual] = useState(new Date()); // Data atual
-
+  // Definição das tarefas
   const [tarefas, setTarefas] = useState([
     { id: 1, nome: "Qualificacao", prazoMeses: 24, descricao: "Apresentação e defesa do projeto de pesquisa.", feita: false },
     { id: 2, nome: "Artigo", prazoMeses: 24, descricao: "Elaborar e submeter um artigo científico.", feita: false },
@@ -20,6 +23,29 @@ function PerfilAluno() {
     { id: 5, nome: "Exame de Proficiência em Língua Estrangeira", prazoMeses: 16, descricao: "Aprovação em exame de proficiência em língua estrangeira.", feita: false },
     { id: 6, nome: "Carga Horária Básica", prazoMeses: 15, descricao: "Cumprir a carga horária mínima de disciplinas obrigatórias.", feita: false },
   ]);
+
+  const idAluno = [
+    { id: 1, nome: 'João Silva', matricula: '2022001', titulacao: 'Mestrado', datainicio: '2023-01-01', orientador:'Frederico Durão' },
+  ];
+
+    // Definindo as datas de início e atual
+    const getData = new Date(idAluno[0].datainicio);
+    const dataDeInicio =  new Date(getData.getFullYear(), getData.getMonth(), getData.getDate() +1 );
+    //const dataFinal = new Date(dataDeInicio.getFullYear(), dataDeInicio.getMonth() + ultimaTarefa.prazoMeses, dataDeInicio.getDate());
+    const [dataAtual, setDataAtual] = useState(new Date());
+    const [aluno, setAluno] = useState(null); // Estado para armazenar os dados do aluno
+    const svgRef = useRef();
+
+        // Definindo a data final com base na titulação do aluno
+    const anosDePrazo = idAluno[0].titulacao === 'Mestrado' ? 2 : 4;
+    const dataFinal = new Date(dataDeInicio);
+    dataFinal.setFullYear(dataFinal.getFullYear() + anosDePrazo);
+  
+
+  // Atualiza o estado do aluno assim que o componente é montado
+  useEffect(() => {
+    setAluno(idAluno[0]); // Define o primeiro aluno da lista
+  }, []);
 
   const [tarefaEmEdicao, setTarefaEmEdicao] = useState(null);
   const [dataSelecionada, setDataSelecionada] = useState(null);
@@ -63,47 +89,70 @@ function PerfilAluno() {
   useEffect(() => {
     const timer = setInterval(() => {
       setDataAtual(new Date());
-    }, 86400000); // Atualiza a data atual todos os dias
+    }, 86400000); // Atualiza data atual diariamente
     return () => {
       clearInterval(timer);
     };
   }, []);
 
+
+
   return (
     <div className="contain">
         <div className="containerAluno">
-          <div className="infoAluno">
+        <div className="infoAluno">
           <img src={logoPgcop} alt="Logo" />
-          <div className="boxInfoAluno">
-            <h3>José  Silva José Silva</h3>
-            <p><span>Titulação:</span> Mestrado/Doutorado</p>
-            <p><span>Data de Inicio:</span> {dataDeInicio.toLocaleDateString()}</p>
-            <p><span>Status:</span> Ativo</p>
-          </div>
+          {aluno && ( // Verifica se o aluno foi carregado
             <div className="boxInfoAluno">
-              <h3><span>Matrícula:</span> xxxxxxxxx</h3>
-              <p><span>Orientador(a): </span>Augusto Carlos</p>
-              <p><span>Término Previsto:</span> {new Date(dataDeInicio.getFullYear() + 3, dataDeInicio.getMonth(), dataDeInicio.getDate()).toLocaleDateString()}</p>
+              <h3>{aluno.nome}</h3>
+              <p><span>Titulação:</span> {aluno.titulacao}</p>
+              <p><span>Data de Inicio:</span> {new Date(dataDeInicio).toLocaleDateString()}</p>
+              <p><span>Status:</span> Ativo</p>
             </div>
+          )}
+          <div className="boxInfoAluno">
+            <h3><span>Matrícula:</span> {aluno && aluno.matricula}</h3>
+            <p><span>Orientador(a): </span>{aluno && aluno.orientador}</p>
+            <p><span>Término Previsto:</span> {new Date(dataFinal).toLocaleDateString()}</p>
           </div>
+        </div>
 
-            <div className="botoesToolbar">
-              <MdEditNote onClick={() => window.location.href = "/perfil-aluno/atualizar-dados"} 
-                style={{ cursor: 'pointer', marginRight:"40px" }} 
-                size={35} 
-                title="Atualizar dados" 
-              />
-              <MdLogout onClick={() => window.location.href = "/"} 
-                style={{ cursor: 'pointer', marginRight:"40px" }} 
-                size={35} 
-                title="Sair" 
-              />
-            </div>
+        <div className="botoesToolbar">
+          <MdEditNote onClick={() => window.location.href = "/perfil-aluno/atualizar-dados"} 
+            style={{ cursor: 'pointer', marginRight:"40px" }} 
+            size={35} 
+            title="Atualizar dados" 
+          />
+          <MdLogout onClick={() => window.location.href = "/"} 
+            style={{ cursor: 'pointer', marginRight:"40px" }} 
+            size={35} 
+            title="Sair" 
+          />
+        </div>
+      </div>
+
+
+      {/* Visualização */}
+      < D3Visualization 
+            dataDeInicio={dataDeInicio} 
+            dataFinal={dataFinal} 
+            dataAtual={dataAtual} 
+            tarefas={tarefas}
+          /> 
+        {/* Div Mouseover */}
+        <div id="tooltip" 
+              style={{ display: "none", 
+                        position: "fixed", 
+                        backgroundColor: "white", 
+                        padding: "5px", 
+                        border: "1px solid black", 
+                        borderEndEndRadius:"15px"
+                    }}>
         </div>
 
 
-      <div className="tarefasAluno" >
-        <div className="boxTarefas" >
+        <div className="tarefasAluno">
+        <div className="boxTarefas">
           <h3 style={{ textAlign: "center" }}>TAREFAS A FAZER</h3>
           {tarefasAFazer.map((tarefa) => {
             const prazo = new Date(dataDeInicio.getFullYear(), dataDeInicio.getMonth() + tarefa.prazoMeses, dataDeInicio.getDate());
@@ -118,14 +167,14 @@ function PerfilAluno() {
             }
 
             return (
-              <div id="task" key={tarefa.id} style={{ backgroundColor: backgroundColor,}}>
+              <div id="task" key={tarefa.id} style={{ backgroundColor: backgroundColor }}>
                 <AiOutlineEdit // Marcador icone
                   onClick={() => handleCheckboxChange(tarefa.id)}
                   style={{ cursor: 'pointer', marginLeft: '5px', marginRight: "10px" }}
                   size={20}  
                   title="Editar"
                 />
-                <label style={{marginLeft: "5px",fontSize: "18px",fontWeight: "500",}}>
+                <label style={{ marginLeft: "5px", fontSize: "18px", fontWeight: "500" }}>
                   {tarefa.nome}
                 </label>
                 {tarefaEmEdicao === tarefa.id && (
@@ -140,16 +189,16 @@ function PerfilAluno() {
                         style={{ marginLeft: "25px" }}
                       />
                       <button onClick={() => salvarDataRealizacao(tarefa.id)} 
-                      style={{marginLeft:"25px",width:'70px', height:'25px', borderRadius: '5px', fontSize: "13px"}}>Salvar</button>
+                        style={{ marginLeft:"25px", width:'70px', height:'25px', borderRadius: '5px', fontSize: "13px" }}>Salvar</button>
                     </label>
                   </>
                 )}
-                <br></br>
+                <br />
                 <label style={{ marginLeft: "40px", fontSize: "15px" }}>
-                  {tarefa.descricao}<br></br>
+                  {tarefa.descricao}<br />
                 </label>
                 <label style={{ marginLeft: "40px", fontSize: "15px" }}>
-                  Data Limite: {prazo.toLocaleDateString()} - {diasRestantes} dias restantes
+                  Data Limite: {prazo.toLocaleDateString()} — {diasRestantes} dias restantes.
                 </label>
               </div>
             );
@@ -158,29 +207,34 @@ function PerfilAluno() {
 
         <div className="boxTarefas">
           <h3 style={{ textAlign: "center" }}>TAREFAS REALIZADAS</h3>
-          {tarefasFeitas.map((tarefa) => {
-            const prazo = new Date(dataDeInicio.getFullYear(), dataDeInicio.getMonth() + tarefa.prazoMeses, dataDeInicio.getDate());
-            return (
-              <div id="task" key={tarefa.id}style={{backgroundColor: "#ADD8E6",}}>
-                <AiOutlineFileExcel // Marcador icone
-                  onClick={() => handleCheckboxChange(tarefa.id)}
-                  style={{ cursor: 'pointer' , marginLeft: "5px"  }}
-                  size={20} 
-                  title="Desfazer"
-                />
-                <label style={{marginLeft: "15px",fontSize: "18px",fontWeight: "500",}}>
-                  {tarefa.nome}
-                </label>
-                <br></br>
-                <label style={{ marginLeft: "40px", fontSize: "15px" }}>
-                  {tarefa.descricao}<br></br>
-                </label>
-                <label style={{ marginLeft: "40px", fontSize: "15px" }}>
+          {tarefasFeitas.length === 0 ? (
+            <p style={{ textAlign: "center", marginTop:"70px",  fontSize: "22px", fontWeight: "400", color: "#A9A9A9" }}>
+              AINDA NÃO HÁ TAREFAS REALIZADAS</p>
+          ) : (
+            tarefasFeitas.map((tarefa) => {
+              const prazo = new Date(dataDeInicio.getFullYear(), dataDeInicio.getMonth() + tarefa.prazoMeses, dataDeInicio.getDate());
+              return (
+                <div id="task" key={tarefa.id} style={{ backgroundColor: "#ADD8E6" }}>
+                  <AiOutlineFileExcel // Marcador icone
+                    onClick={() => handleCheckboxChange(tarefa.id)}
+                    style={{ cursor: 'pointer', marginLeft: "5px" }}
+                    size={20} 
+                    title="Desfazer"
+                  />
+                  <label style={{ marginLeft: "15px", fontSize: "18px", fontWeight: "500" }}>
+                    {tarefa.nome}
+                  </label>
+                  <br />
+                  <label style={{ marginLeft: "40px", fontSize: "15px" }}>
+                    {tarefa.descricao}<br />
+                  </label>
+                  <label style={{ marginLeft: "40px", fontSize: "15px" }}>
                     Realizada em: {new Date(tarefa.dataRealizacao).toLocaleDateString()}
-                </label>
-              </div>
-            );
-          })}
+                  </label>
+                </div>
+              );
+            })
+          )}
         </div>
       </div>
     </div>
