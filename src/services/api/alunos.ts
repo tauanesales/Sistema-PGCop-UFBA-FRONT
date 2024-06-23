c559bbc8cb2006736569b2ba0ef8f894ccb0fc22
Pablo Henrique <pablohenrique.r.c@gmail.com>
2024-06-23 16:03:13 -0300

    Remove código de debug indesejado

--- src/services/api/alunos.ts
+++ src/services/api/alunos.ts
@@ -11,5 +11,5 @@ type UpdateAlunoParams = Partial<Aluno> & Pick<Aluno, "id">;
 
 export const updateAluno = ({ id, ...aluno }: UpdateAlunoParams) =>
   api
-    .put<Aluno>(`/alunos/${id}`, { ...aluno, nome: "Teste" })
+    .put<Aluno>(`/alunos/${id}`, { ...aluno })
     .then((response) => response.data);
