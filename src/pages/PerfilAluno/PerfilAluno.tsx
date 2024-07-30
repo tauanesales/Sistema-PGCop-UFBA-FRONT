0d12fefd02347ae3ea260db42a562a92c85fa08a
Letícia Costa <leticiamc@ufba.br>
2024-07-30 15:22:55 -0300

    atualizacoes tela perfil aluno

--- src/pages/PerfilAluno/PerfilAluno.tsx
+++ src/pages/PerfilAluno/PerfilAluno.tsx
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 import { differenceInDays, format } from "date-fns";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AiOutlineEdit, AiOutlineFileExcel } from "react-icons/ai";
 import { MdEditNote, MdLogout } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
@@ -12,6 +12,7 @@ import { Aluno, Curso } from "@/models/User";
 import { useProfessoresQueries } from "@/queries/professores";
 import { useTarefasQueries } from "@/queries/tarefas";
 import { useUserQueries } from "@/queries/user";
+import  {Stack, Row, Col, Container, Navbar, Card, Alert, Button, Form} from "react-bootstrap";
 
 function calculateDifferenceInMonthsAndDays(startDate: Date, endDate: Date) {
   const totalDays = differenceInDays(endDate, startDate);
@@ -45,6 +46,11 @@ function PerfilAluno() {
   const [dataSelecionada, setDataSelecionada] = useState(
     new Date().toISOString().split("T")[0],
   );
+
+  useEffect(() => {
+    console.log(user)
+  }
+  , [user])
 
   const handleCheckboxChange = (tarefa: Tarefa) => {
     const { id, completada } = tarefa;
@@ -83,60 +89,71 @@ function PerfilAluno() {
   const tarefasFeitas = tarefas.filter((tarefa) => tarefa.completada);
 
   return (
-    <div className="contain">
-      <div className="containerAluno">
+    <div className="contain" >
         {user && (
-          <div className="infoAluno">
-            <img src={logoPgcop} alt="Logo" />
-            <div className="boxInfoAluno">
-              <h3>{user.nome}</h3>
-              <p>
-                <span>Titulação:</span> {Curso[user.curso]}
-              </p>
-              <p>
-                <span>Data de Inicio:</span>{" "}
-                {user.data_ingresso
-                  ? format(new Date(user.data_ingresso), "dd/MM/yyyy")
-                  : "-"}
-              </p>
-              <p>
-                <span>Status:</span>{" "}
-                {tarefasAFazer.length > 0 ? "Ativo" : "Concluído"}
-              </p>
-            </div>
-            <div className="boxInfoAluno">
-              <h3>
-                <span>Matrícula:</span> {user.matricula}
-              </h3>
-              <p>
-                <span>Orientador(a): </span>
-                {nomeOrientador}
-              </p>
-              <p>
-                <span>Término Previsto:</span>{" "}
-                {user.data_qualificacao
-                  ? format(new Date(user.data_qualificacao), "dd/MM/yyyy")
-                  : "-"}
-              </p>
-            </div>
-          </div>
-        )}
+          <Navbar>
+            <Container fluid className="containerAluno bg-body-tertiary">
+              <Navbar.Brand href="/perfil-aluno">
+                <img src={logoPgcop} alt="Logo" />
+              </Navbar.Brand>
+              <Stack direction="horizontal" gap={3} className="boxAluno">
+                    <Stack className="boxInfoAluno">
+                      <h3><span>{user.nome}</span></h3>
+                      <p>
+                        <span>Titulação:</span> {Curso[user.curso]}
+                      </p>
+                      <p>
+                        <span>Data de início:</span>{" "}
+                          {user.data_ingresso
+                            ? format(new Date(user.data_ingresso), "dd/MM/yyyy")
+                            : "-"}
+                      </p>
+                      <p>
+                        <span>Status:</span>{" "}
+                        {tarefasAFazer.length > 0 ? "Ativo" : "Concluído"}
+                      </p>
+                    </Stack>
+                    <Stack className="boxInfoAluno">
+                      <h3>
+                        <span>Matrícula:</span> {user.matricula}
+                      </h3>
+                      <p>
+                        <span>Orientador(a): </span>
+                        {nomeOrientador}
+                      </p>
+                      <p>
+                        <span>Defesa prevista:</span>{" "}
+                        {user.data_qualificacao
+                          ? format(new Date(user.data_qualificacao), "dd/MM/yyyy")
+                          : "-"}
+                      </p>
+                      <p>
+                        <span>Lattes:</span>
+                        <a href={user.lattes} target="_blank" rel="noopener noreferrer">
+                          {user.lattes}
+                        </a>
+                      </p>
+                    </Stack>
+                    <Stack className="botoesToolbarAluno">
+                      <MdEditNote
+                      onClick={() => navigate("/perfil-aluno/editar-dados")}
+                      style={{ cursor: "pointer", marginRight: "40px" }}
+                      size={35}
+                      title="Editar dados"
+                      />
+                      <MdLogout
+                        onClick={signOut}
+                        style={{ cursor: "pointer", marginRight: "40px" }}
+                        size={35}
+                        title="Sair"
+                      />
+                    </Stack>
 
-        <div className="botoesToolbarAluno">
-          <MdEditNote
-            onClick={() => navigate("/perfil-aluno/atualizar-dados")}
-            style={{ cursor: "pointer", marginRight: "40px" }}
-            size={35}
-            title="Atualizar dados"
-          />
-          <MdLogout
-            onClick={signOut}
-            style={{ cursor: "pointer", marginRight: "40px" }}
-            size={35}
-            title="Sair"
-          />
-        </div>
-      </div>
+              </Stack>
+            </Container>
+          </Navbar>
+        )}
+        
 
       {/* Visualização */}
       {user.data_qualificacao && (
@@ -161,13 +178,17 @@ function PerfilAluno() {
         }}
       ></div>
 
+
       <div className="tarefasAluno">
-        <div className="boxTarefas">
-          <h3 style={{ textAlign: "center" }}>TAREFAS A FAZER</h3>
-          {tarefasAFazer.map((tarefa) => {
-            const prazo = new Date(tarefa.data_prazo);
-            const { months, days, totalDays } =
-              calculateDifferenceInMonthsAndDays(new Date(), prazo);
+        <Card className="boxTarefas">
+          <Card.Body>
+            <Card.Title className="titleTarefas">Tarefas a fazer</Card.Title>
+            {tarefasAFazer.map((tarefa) => {
+                const prazo = new Date(tarefa.data_prazo);
+                const { months, days, totalDays } = calculateDifferenceInMonthsAndDays(
+                  new Date(),
+                  prazo
+                );
 
             let backgroundColor;
             if (totalDays < 0) {
@@ -195,123 +216,109 @@ function PerfilAluno() {
                       }`
                   : `${-totalDays} dia${-totalDays > 1 ? "s" : ""} em atraso`;
 
-            return (
-              <div
-                id="task"
-                key={tarefa.id}
-                style={{ backgroundColor: backgroundColor }}
-              >
-                <AiOutlineEdit // Marcador icone
-                  onClick={() => handleCheckboxChange(tarefa)}
-                  style={{
-                    cursor: "pointer",
-                    marginLeft: "5px",
-                    marginRight: "10px",
-                  }}
-                  size={20}
-                  title="Editar"
-                />
-                <label
-                  style={{
-                    marginLeft: "5px",
-                    fontSize: "18px",
-                    fontWeight: "500",
-                  }}
-                >
-                  {tarefa.nome}
-                </label>
-                {tarefaEmEdicao === tarefa.id && (
-                  <>
-                    <br />
-                    <label style={{ marginLeft: "40px", fontSize: "15px" }}>
-                      Data de realização:
-                      <input
-                        className="dateInput"
-                        type="date"
-                        value={dataSelecionada}
-                        onChange={(e) => setDataSelecionada(e.target.value)}
-                        style={{ marginLeft: "15px" }}
-                      />
-                      <button
-                        className="bttnSalvar"
-                        onClick={() => salvarDataRealizacao(tarefa)}
+                return (
+                  <Alert style={{backgroundColor: backgroundColor, color: "black"}} id="task"
+                    key={tarefa.id}>
+                      <AiOutlineEdit // Marcador icone
+                        onClick={() => handleCheckboxChange(tarefa)}
                         style={{
-                          marginLeft: "12vh",
-                          width: "70px",
-                          height: "25px",
+                          cursor: "pointer",
+                          marginLeft: "5px",
+                          marginRight: "10px",
+                        }}
+                        size={20}
+                        title="Editar"
+                      />
+                      <label
+                        style={{
+                          marginLeft: "5px",
+                          fontSize: "18px",
+                          fontWeight: "500",
                         }}
                       >
-                        Salvar
-                      </button>
-                    </label>
-                  </>
-                )}
-                <br />
-                <label style={{ marginLeft: "40px", fontSize: "15px" }}>
-                  {tarefa.descricao}
-                  <br />
-                </label>
-                <label style={{ marginLeft: "40px", fontSize: "15px" }}>
-                  Data Limite: {format(prazo, "dd/MM/yyyy")} - {statusData}
-                </label>
-              </div>
-            );
-          })}
-        </div>
+                        {tarefa.nome}
+                      </label>
+                      {tarefaEmEdicao === tarefa.id && (
+                        <Form.Group className="boxDate">
+                          <Form.Label>Data de realização:</Form.Label>
+                          <Form.Control
+                            size="sm"   
+                            type="date" 
+                            className="dateInput" 
+                            value={dataSelecionada}
+                            onChange={(e) => setDataSelecionada(e.target.value)}/>
+                          <Button variant="primary" className="bttnSalvar" onClick={() => salvarDataRealizacao(tarefa)}>Salvar</Button>
+                        </Form.Group>
+                      )}
+                      <label style={{ marginLeft: "40px", fontSize: "15px" }}>
+                        {tarefa.descricao}
+                      </label>
+                      <label style={{ marginLeft: "40px", fontSize: "15px" }}>
+                        Data Limite: {format(prazo, "dd/MM/yyyy")} - {statusData}
+                      </label>
 
-        <div className="boxTarefas">
-          <h3 style={{ textAlign: "center" }}>TAREFAS REALIZADAS</h3>
-          {tarefasFeitas.length === 0 ? (
-            <p
-              style={{
-                textAlign: "center",
-                marginTop: "70px",
-                fontSize: "25px",
-                fontWeight: "600",
-                color: "lightgrey",
-              }}
-            >
-              AINDA NÃO HÁ TAREFAS REALIZADAS
-            </p>
-          ) : (
-            tarefasFeitas.map((tarefa) => {
-              return (
-                <div
-                  id="task"
-                  key={tarefa.id}
-                  style={{ backgroundColor: "#b2e6ad" }}
+                  </Alert>
+                );
+              })}
+          </Card.Body>
+        </Card>
+        <Card className="boxTarefas">
+          <Card.Body>
+            <Card.Title className="titleTarefas">Tarefas concluídas</Card.Title>
+            <Card.Text>
+            {tarefasFeitas.length === 0 ? (
+                <p
+                  style={{
+                    textAlign: "center",
+                    marginTop: "70px",
+                    fontSize: "25px",
+                    fontWeight: "600",
+                    color: "lightgrey",
+                  }}
                 >
-                  <AiOutlineFileExcel // Marcador icone
-                    onClick={() => handleCheckboxChange(tarefa)}
-                    style={{ cursor: "pointer", marginLeft: "5px" }}
-                    size={20}
-                    title="Desfazer"
-                  />
-                  <label
-                    style={{
-                      marginLeft: "15px",
-                      fontSize: "18px",
-                      fontWeight: "500",
-                    }}
-                  >
-                    {tarefa.nome}
-                  </label>
-                  <br />
-                  <label style={{ marginLeft: "40px", fontSize: "15px" }}>
-                    {tarefa.descricao}
-                    <br />
-                  </label>
-                  <label style={{ marginLeft: "40px", fontSize: "15px" }}>
-                    Realizada em:{" "}
-                    {tarefa.data_conclusao
-                      ? format(new Date(tarefa.data_conclusao), "dd/MM/yyyy")
-                      : "-"}
-                  </label>
-                </div>
-              );
-            })
-          )}
-        </div>
+                  Ainda não existem tarefas concluídas
+                </p>
+              ) : (
+                tarefasFeitas.map((tarefa) => {
+                  return (
+                    <div
+                      id="task"
+                      key={tarefa.id}
+                      style={{ backgroundColor: "#b2e6ad" }}
+                    >
+                      <AiOutlineFileExcel // Marcador icone
+                        onClick={() => handleCheckboxChange(tarefa)}
+                        style={{ cursor: "pointer", marginLeft: "5px" }}
+                        size={20}
+                        title="Desfazer"
+                      />
+                      <label
+                        style={{
+                          marginLeft: "15px",
+                          fontSize: "18px",
+                          fontWeight: "500",
+                        }}
+                      >
+                        {tarefa.nome}
+                      </label>
+                      <br />
+                      <label style={{ marginLeft: "40px", fontSize: "15px" }}>
+                        {tarefa.descricao}
+                        <br />
+                      </label>
+                      <label style={{ marginLeft: "40px", fontSize: "15px" }}>
+                        Realizada em:{" "}
+                        {tarefa.data_conclusao
+                          ? format(new Date(tarefa.data_conclusao), "dd/MM/yyyy")
+                          : "-"}
+                      </label>
+                    </div>
+                  );
+                })
+              )}
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     </div>
   );
